8bb45c07b336fc9a400c0c25d1c8042aa666d0f8
Hussein Alwesi <Hussein.Al-wesi@iHorizons.com>
2025-06-29 19:56:28 +0300

    setup primeNG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, ButtonModule, TableModule, InputTextModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'genovation-ai';
+  items = [
+    { brand: 'Toyota', year: 2020 },
+    { brand: 'Ford', year: 2018 }
+  ];
 }
