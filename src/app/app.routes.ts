7a611111800c151107ae652500071b3fba3c2544
Hussein Alwesi <Hussein.Al-wesi@iHorizons.com>
2025-06-30 17:58:45 +0300

    initialize pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ReportComponent } from './pages/report/report.component';
+import { UserListComponent } from './pages/user-list/user-list.component';
+import { UserAddComponent } from './pages/user-add/user-add.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: DashboardComponent },
+    { path: 'report', component: ReportComponent },
+    { path: 'user-list', component: UserListComponent },
+    { path: 'user-add', component: UserAddComponent },
+    { path: '404', component: NotFoundComponent },    
+    { path: '**', redirectTo: '404' }
+];
--- src/app/modules/layout-components/main-content/main-content.component.ts
+++ src/app/modules/layout-components/main-content/main-content.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
