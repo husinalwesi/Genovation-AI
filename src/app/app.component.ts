e257323b289ff0b89d93326035e04f45055d8a85
Hussein Alwesi <Hussein.Al-wesi@iHorizons.com>
2025-06-30 17:04:59 +0300

    enhance layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,76 +1,48 @@
 import { CommonModule } from '@angular/common';
 import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
-import { ButtonModule } from 'primeng/button';
-import { RippleModule } from 'primeng/ripple';
 import { Sidebar, SidebarModule } from 'primeng/sidebar';
-import { ToolbarModule } from 'primeng/toolbar';
-import { PanelMenuModule } from 'primeng/panelmenu';
 import { MenubarModule } from 'primeng/menubar';
-import { BadgeModule } from 'primeng/badge';
+import { ButtonModule } from 'primeng/button';
 import { AvatarModule } from 'primeng/avatar';
-import { InputTextModule } from 'primeng/inputtext';
 
 const MOBILE_BREAKPOINT = 768;
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    SidebarModule,
-    ButtonModule,
-    RippleModule,
-    ToolbarModule,
-    PanelMenuModule,
-    MenubarModule,
-    BadgeModule,
-    AvatarModule,
-    InputTextModule,
-  ],
+  imports: [CommonModule, SidebarModule, MenubarModule, ButtonModule, AvatarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
-  isMobile: boolean = false;
-  fullView: boolean = true;
-
-  ngOnInit() {
-    this.detectDeviceSize();
-    this.defineView();
-  }
-
   @ViewChild('sidebarRef') sidebarRef!: Sidebar;
 
-  sidebarVisible: boolean = false;
+  isMobile = false;
+  sidebarVisible = false;
+  expandedSidebar = true;
 
-  detectDeviceSize() {
-    const tempIsMobile = window.innerWidth < MOBILE_BREAKPOINT;
-    const isSwitchFromMobToDeskOrViseVersa = tempIsMobile !== this.isMobile;
-    this.isMobile = tempIsMobile;
-    if ((isSwitchFromMobToDeskOrViseVersa || this.isMobile) && this.sidebarRef)
-      this.sidebarRef.destroyModal();
+  ngOnInit() {
+    this.updateLayout();
   }
 
-  @HostListener('window:resize', ['$event']) onWindowResize() {
-    this.onResize();
-  }
-
+  @HostListener('window:resize')
   onResize() {
-    this.detectDeviceSize();
-    this.defineView();
+    this.updateLayout();
   }
 
-  defineView() {
+  private updateLayout() {
+    const wasMobile = this.isMobile;
+    this.isMobile = window.innerWidth < MOBILE_BREAKPOINT;
     this.sidebarVisible = !this.isMobile;
+
+    if (wasMobile !== this.isMobile && this.sidebarRef) this.sidebarRef.destroyModal();
   }
 
-  toggleDesktopMenu() {
-    this.fullView = !this.fullView;
-    if (this.sidebarVisible) this.refresh();
-  }
+  toggleSidebarWidth() {
+    this.expandedSidebar = !this.expandedSidebar;
 
-  refresh() {
+    // Soft re-render
     this.sidebarVisible = false;
-    setTimeout(() => (this.sidebarVisible = true), 0);
+    setTimeout(() => (this.sidebarVisible = true));
   }
 }
